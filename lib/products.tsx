2ba6cb3faf79bfb901988fa65684a7fcddbd957b
Max <bazarsadtsogtjargal@gmail.com>
2021-12-07 21:37:42 +0900

    add product page and fallback

--- lib/products.tsx
+++ lib/products.tsx
@@ -1,17 +1,29 @@
+import { fetchJson } from './api';
+
+const CMS_URL = 'http://localhost:1337';
 export interface Product {
 	id: number;
 	title: string;
+	description: string;
+}
+
+
+
+export async function getProduct(id: string): Promise<Product> {
+
+	const product = await fetchJson(`${CMS_URL}/products/${id}`);
+	return stripProduct(product);
+}
+
+export async function getProducts(): Promise<Product[]> {
+	const products = await fetchJson(`${CMS_URL}/products`);
+	return products.map(stripProduct);
 }
 
 function stripProduct(product: any): Product {
 	return {
 		id: product.id,
 		title: product.title,
+		description: product.description,
 	};
-}
-
-export async function getProducts() {
-	const response = await fetch('http://localhost:1337/products');
-	const products = await response.json();
-	return products.map(stripProduct);
 }
---
+++ pages/products/[id].tsx
@@ -0,0 +1,55 @@
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { ParsedUrlQuery } from 'querystring';
+import Title from '../../components/Title';
+import { getProduct, getProducts, Product } from '../../lib/products';
+
+interface ProductPageParams extends ParsedUrlQuery {
+    id: string;
+}
+
+interface ProductPageProps {
+    product: Product;
+}
+
+export const getStaticPaths: GetStaticPaths<ProductPageParams> = async () => {
+    const products = await getProducts();
+    return {
+        paths: products.map((product) => ({
+            params: { id: product.id.toString() },
+        })),
+        fallback: 'blocking',
+    };
+};
+
+export const getStaticProps: GetStaticProps<
+    ProductPageProps,
+    ProductPageParams
+> = async ({ params: { id } }) => {
+    try {
+        const product = await getProduct(id);
+        return {
+            props: { product },
+            revalidate: 30,
+        };
+    } catch (error) {
+        return { notFound: true, }
+    }
+
+};
+
+const ProductPage: React.FC<ProductPageProps> = ({ product }) => {
+    return (
+        <>
+            <Head>
+                <title>Id</title>
+            </Head>
+            <main>
+                <Title>{product.title}</Title>
+                <p>{product.description}</p>
+            </main>
+        </>
+    );
+};
+
+export default ProductPage;
