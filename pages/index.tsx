82ed461cc6bff2474c60c094befd8b046f8950cb
Max <bazarsadtsogtjargal@gmail.com>
2021-12-07 20:19:53 +0900

    setting up data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,44 @@
+// Option 1: fetch products on the server side (getStaticProps)
+
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 import Title from '../components/Title';
+import { getProducts, Product } from '../lib/products';
 
-const HomePage: React.FC = () => {
+interface HomePageProps {
+	products: Product[];
+}
+
+export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
+	console.log('[HomePage] getStaticProps()');
+	const products = await getProducts();
+	return {
+		props: { products },
+		revalidate: 30,
+	};
+};
+
+const HomePage: React.FC<HomePageProps> = ({ products }) => {
+	console.log('[HomePage - 1] render', products);
 	return (
 		<>
 			<Head>
 				<title>Next Shop</title>
 			</Head>
-			<main className='px-6 py-6'>
-				<Title>Next Shop</Title>
 
-				<p>[TODO: display products]</p>
+			<main className='px-6 py-4'>
+				<Title>Next Shop</Title>
+				<ul>
+					{products.map((product) => (
+						<li key={product.id}>
+							<Link href={`/products/${product.id}`}>
+								<a>{product.title}</a>
+							</Link>
+						</li>
+					))}
+				</ul>
 			</main>
 		</>
 	);
