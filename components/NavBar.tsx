a4bc201364acceb7317c59f8570b99c1966f7012
Max <bazarsadtsogtjargal@gmail.com>
2021-12-08 22:25:16 +0900

    adding navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,55 @@
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { fetchJson } from '../lib/api';
+import { User } from '../lib/user';
+
+
+const NavBar: React.FC = () => {
+    const [user, setUser] = useState<User>();
+    useEffect(() => {
+        (async () => {
+            try {
+                const user = await fetchJson('/api/user');
+                setUser(user);
+            } catch (err) {
+                // not signed in
+            }
+        })();
+    }, []);
+
+    console.log('[NavBar] user:', user);
+    return (
+        <nav className="px-2 py-1 text-sm">
+            <ul className="flex gap-2">
+                <li className="text-lg font-extrabold">
+                    <Link href="/">
+                        <a>
+                            Next Shop
+                        </a>
+                    </Link>
+                </li>
+                <li role="separator" className="flex-1" />
+                {user ? (
+                    <>
+                        <li>
+                            {user.name}
+                        </li>
+                        <li>
+                            <button>Sign Out</button>
+                        </li>
+                    </>
+                ) : (
+                    <li>
+                        <Link href="/sign-in">
+                            <a>
+                                Sign In
+                            </a>
+                        </Link>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
